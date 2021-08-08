aa747c250e13c2ca4de039bf4ea72ff4ece60a7c
MarvinHezron <marvinrozen@gmail.com>
2021-08-08 16:49:03 +0300

    express asset refference

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
 var absolutePath = __dirname + "/views/index.html";
 app.get("/", function(req, res){
   res.sendFile(absolutePath);
