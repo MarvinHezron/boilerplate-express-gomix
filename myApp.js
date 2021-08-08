4f36aab7cdfade110ee53544c61a8d5e414153c4
MarvinHezron <marvinrozen@gmail.com>
2021-08-08 13:58:03 +0300

    sendFile

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
+var absolutePath = __dirname + "/views/index.html";
 app.get("/", function(req, res){
-    res.send("Hello Express");
-  });
+  res.sendFile(absolutePath);
+});
 
 
 
